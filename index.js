98f5e228db4f8d06c7aa106dd310fb5cac7bf500
sanjay128yadav <sanjay128yadav@gmail.com>
2024-10-19 23:43:47 +0530

    BNM #19102024

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ const publicPath = path.join(__dirname, 'public');
 
 const app = express();
 
-// Code here to load static content pages
-app.use(express.static(publicPath));
+// Method1 Code here to load static content pages
+// app.use(express.static(publicPath));
+
+// Method 2 display page without file extension
+
+app.get('', (requ, resp)=>{
+    resp.sendFile(`${publicPath}/index.html`);
+});
+
+app.get('/about', (requ, resp)=>{
+    resp.sendFile(`${publicPath}/about.html`);
+});
+
+app.get('*', (requ, resp)=>{
+    resp.sendFile(`${publicPath}/nopage.html`);
+});
 
 app.listen(4200);
