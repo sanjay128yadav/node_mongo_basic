f497d46502a3d800866a3c4a74be34fda38ec28a
sanjay128yadav <sanjay128yadav@gmail.com>
2024-10-27 23:57:29 +0530

    basic api #27102024

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ app.get('', (req, resp) => {
     resp.end();
 });
 
-app.get('/about', (req, resp) => {
-    resp.send('This is about page. Your most welcome Mr.' + req.query.name);
-    console.log('Data send through browser is:'+ req.query.name);
-    resp.end();
-});
+
 
 app.listen(4500);
