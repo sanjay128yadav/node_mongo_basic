f44802916c00bb2a253d18c4798c87ccb0a7a840
sanjay128yadav <sanjay128yadav@gmail.com>
2024-10-29 23:26:37 +0530

    API mongoose #29102024

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 
+require('./config');
+
+const Product = require('./product');
+
 const app = express();
 
-app.get('', (req, resp) => {
-    resp.send('Home page');
-    resp.end();
-});
+app.use(express.json());
 
-app.get('/about', (req, resp) => {
-    resp.send('This is about page. Your most welcome Mr.' + req.query.name);
-    console.log('Data send through browser is:'+ req.query.name);
-    resp.end();
+app.post('/create', async(req, resp) => {  
+    
+    let data = new Product(req.body);
+    let result  = await data.save();
+    resp.send(result);
 });
 
 app.listen(4500);
