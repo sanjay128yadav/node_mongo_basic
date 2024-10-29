3d0b42c7630b0d44050e610914f83842799d47a2
sanjay128yadav <sanjay128yadav@gmail.com>
2024-10-29 23:58:41 +0530

    API mongoose #29102024

--- index.js
+++ index.js
@@ -15,4 +15,21 @@ app.post('/create', async(req, resp) => {
     resp.send(result);
 });
 
+app.get('/list', async(req, resp)=>{
+    let data = await Product.find();
+    resp.send(data);
+});
+
+app.delete('/delete/:_id', async(req, resp)=>{   
+    let result = await Product.deleteOne(req.params);
+    resp.send(result);
+});
+
+app.put('/update/:_id', async(req, resp)=>{   
+    let result = await Product.updateOne(req.params, {$set: req.body});
+    resp.send(result);
+});
+
+
+
 app.listen(4500);
